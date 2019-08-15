5f62e042e303f9f40abed14071f1040b710741b3
cookieshake <cookieshake.dev@gmail.com>
2019-08-15 23:12:34 +0900

    add log for stan

--- clients.go
+++ clients.go
@@ -1,11 +1,13 @@
 package receiver
 
 import (
+	"fmt"
 	"github.com/labstack/gommon/random"
 	"github.com/nats-io/stan.go"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 	"os"
+	"github.com/rs/zerolog/log"
 )
 
 func newStanClient(config *viper.Viper) (stan.Conn, error) {
@@ -19,6 +21,7 @@ func newStanClient(config *viper.Viper) (stan.Conn, error) {
 	}
 	natsURL := config.GetString("nats_url")
 
+	log.Info().Msg(fmt.Sprintf("creating connection with %s %s %s", clusterID, clientID, natsURL))
 	client, err := stan.Connect(clusterID, clientID, stan.NatsURL(natsURL))
 	if err != nil {
 		return nil, errors.Wrap(err, "creating stan client failed: " + natsURL)
